c8048c01122ae16a6a620e7978deca773eb1f6fc
killshotxd <hassanansari211@gmail.com>
2022-09-06 19:20:25 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Home, Mail, Shuffle } from "react-feather";
 import styles from "./Header.module.css";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
 const Header = () => {
   return (
     <div className={styles.container}>
@@ -8,8 +18,22 @@ const Header = () => {
         <ul>Services</ul>
         <ul>Products</ul>
         <ul>Technology</ul>
-        <ul>About</ul>
-        <ul>Client</ul>
+        <ul>
+          <select name="About" id="about">
+            <option>About</option>
+            <option>About</option>
+            <option>About</option>
+          </select>
+        </ul>
+        <ul>
+          {" "}
+          <select name="Client" id="client">
+            <option>Client</option>
+            <option>About</option>
+            <option>About</option>
+          </select>
+        </ul>
+        <ul>Partner</ul>
       </div>
       <div className={styles.right}>
         <Home />
