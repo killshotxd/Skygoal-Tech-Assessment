8011d2054e8faa2ad5e6b554789baf78eae743ec
killshotxd <hassanansari211@gmail.com>
2022-09-06 18:19:54 +0530

    section 3 complete

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import Section1 from "./Section/1/Section1";
 import Section2 from "./Section/2/Section2";
+import Section3 from "./Section/3/Section3";
 const Body = () => {
   return (
     <>
       <Section1 />
       <Section2 />
+      <Section3 />
     </>
   );
 };
---
+++ src/components/Body/Section/3/Section3.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./Section3.module.css";
+import one from "../../../../assets/product-description 1.png";
+import two from "../../../../assets/product-description 2.png";
+import three from "../../../../assets/product-description 3.png";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+const Section3 = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.centerHeading}>
+        <h2>Product and Service</h2>
+      </div>
+      <div className={styles.containers}>
+        <div className={styles.one}>
+          <img src={one} alt="" />
+          <h3>Our Product</h3>
+          <p>
+            Our product is made on the base of our team's careful research and
+            analyses, ranging from internet based application.
+          </p>
+          <div>
+            <Button colorScheme="red">Read More</Button>
+          </div>
+        </div>
+
+        <div className={styles.two}>
+          <img src={two} alt="" />
+          <h3>Our Service</h3>
+          <p>
+            DSI's shared service solutions focus on the front-end based software
+            development, designed specifically for the banking and financial
+            sectors.
+          </p>
+          <div>
+            <Button colorScheme="red">Read More</Button>
+          </div>
+        </div>
+
+        <div className={styles.three}>
+          <img src={three} alt="" />
+          <h3>Our Technology</h3>
+          <p>
+            First JAVA, runs on more than 850 million personal computers
+            worldwide, and on billions of devices worldwide, including mobile
+            and TV devices.
+          </p>
+          <div>
+            <Button colorScheme="red">Read More</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
